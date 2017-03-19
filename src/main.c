fcf621ca5e9794f541a22396b1112bc667eb483b
Eugene Morozov <zhe3145@yandex.ru>
2017-03-19 16:40:20 +0300

    Small changes in main.c

--- src/main.c
+++ src/main.c
@@ -1,20 +1,19 @@
 #include <CMSIS/stm32f407xx.h>
- 
+
 #define LED_PIN 5
-#define LED_ON() GPIOA->BSRR |= GPIO_BSRR_BS5
-#define LED_OFF() GPIOA->BSRR &= ~GPIO_BSRR_BS5
- 
-int main(int argc, const char * argv[]) 
+#define LED_ON() GPIOA->BSRR |= GPIO_BSRR_BR13
+#define LED_OFF() GPIOA->BSRR &= ~GPIO_BSRR_BR13
+
+int main(int argc, const char * argv[])
 {
 	/* Enbale GPIOA clock */
-	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
-	/* Configure GPIOA pin 5 as output */
-	GPIOA->MODER |= (1 << (LED_PIN << 1));
-	/* Configure GPIOA pin 5 in max speed */
-	GPIOA->OSPEEDR |= (3 << (LED_PIN << 1));
- 
-	/* Turn on the LED */
-	LED_ON();
-	
-	for ( ; ; ) {}
+    RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
+    /* Configure GPIOA pin 5 as output */
+    GPIOD->MODER |= GPIO_MODER_MODER13;
+    /* Configure GPIOA pin 5 in max speed */
+    GPIOA->OSPEEDR |= GPIO_OSPEEDR_OSPEED13;
+    /* Turn on the LED */
+    LED_ON();
+
+    for ( ; ; ) {}
 }
